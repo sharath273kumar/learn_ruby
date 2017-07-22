aa81f286c0e810cfeb5c32d7a1dc57d4a7efbe58
sharath273kumar <sharath273kumar@gmail.com>
2017-07-22 19:40:28 +0530

    done some

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+def ftoc f
+	(f-32)*5/9
+	
+end
+def ctof c
+	(c*9.0/5)+32
+	
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,9 @@
 #write your code here
+def add a,b
+	a+b
+
+end
+def subtract a,b
+	a-b
+	
+end
